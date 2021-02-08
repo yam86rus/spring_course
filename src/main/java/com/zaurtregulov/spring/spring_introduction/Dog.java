33e80be5ca8490dfac96446f7c05059b0b7f61b5
Yam86rus <yam86rus@gmail.com>
2021-02-08 16:19:52 +0500

    up

--- src/main/java/com/zaurtregulov/spring/spring_introduction/Dog.java
+++ src/main/java/com/zaurtregulov/spring/spring_introduction/Dog.java
@@ -1,8 +1,17 @@
 package com.zaurtregulov.spring.spring_introduction;
 
 public class Dog implements Pet{
+    public String name;
     public Dog() {
         System.out.println("Dog bean is creating");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
